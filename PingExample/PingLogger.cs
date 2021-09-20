89692bc8a60e9d7a0251720359825d4fdf44ecc3
ygreksh <ygreksh@gmail.com>
2021-09-20 23:14:30 +0300

    pinglogger not complete

--- PingExample/PingLogger.cs
+++ PingExample/PingLogger.cs
@@ -1,7 +1,27 @@
+using System;
+using System.IO;
+
 namespace PingExample
 {
     public class PingLogger
     {
-        
+        //private string HostsFileName = "hosts.json";
+        private string LogsFileName = "logs.txt";
+
+        public void WriteLog(string logstring)
+        {
+            try
+            {
+                //bool fExists = File.Exists(distStorage);
+                using (StreamWriter sw = new StreamWriter(LogsFileName, true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLineAsync(logstring);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Ошибка записи логов", e);
+            }
+        }
     }
 }
