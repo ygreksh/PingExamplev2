6269a81cf46f2362f7eb0470eeaa1106c601ca86
ygreksh <ygreksh@gmail.com>
2021-09-25 22:09:46 +0300

    ConfigManagerTests

---
+++ PingExample.Tests/ConfigManagerTests.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace PingExample.Tests
+{
+    public class ConfigManagerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
