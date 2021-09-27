1dfc0615965dfc5559837f481ea5a6ed7b4b53ff
ygrek <ygreksh@gmail.com>
2021-09-27 15:38:52 +0300

    HTTPPinger log only when changed status

--- PingExample/Services/HttpPinger.cs
+++ PingExample/Services/HttpPinger.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text.RegularExpressions;
+using System.Threading;
 using PingExample.Interfaces;
 
 namespace PingExample
@@ -14,11 +15,19 @@ namespace PingExample
         private HttpStatusCode _httpStatusCode;
         private ILogger _logger;
         private IPinger _pingerImplementation;
+        private HttpStatusCode previousStatus;
+        private HttpStatusCode currentStatus;
+        private bool changedStatus = false;
+        private bool isStarted = false;
+        private int Period;
+        private DateTime previousDateTime;
+        private DateTime currentDateTime;
 
         public HttpPinger(PingHost pingHost)
         {
             Host = pingHost.Host;
             _httpStatusCode = pingHost.StatusCode;
+            Period = pingHost.Period;
         }
         
         public void SetLogger(ILogger logger)
@@ -33,7 +42,8 @@ namespace PingExample
             string FullHost;
             using (HttpClient client = new HttpClient())
             {
-                DateTime now = DateTime.Now;
+                
+                 currentDateTime = DateTime.Now;
                 try
                 {
                     if (!IPAddress.TryParse(Host, out Ip))
@@ -49,23 +59,42 @@ namespace PingExample
                     {
                         FullHost = Host;
                     }
+
+                    
+                    //первый пинг
                     responseMessage = await client.GetAsync(FullHost);
-                    if (responseMessage.IsSuccessStatusCode)
+                    if (!isStarted)
                     {
-                        Console.WriteLine($"{now.ToString("yyyy/MM/dd hh:mm:ss")} HTTP Connect to {Host} ({Ip}) success");
-                        _logger.WriteLog($"{now.ToString("yyyy/MM/dd hh:mm:ss")} HTTP Connect to {Host} ({Ip}) success");
+                        currentStatus = responseMessage.StatusCode;
+                        isStarted = true;
                     }
+                    previousStatus = responseMessage.StatusCode;
+                    if (previousStatus == currentStatus)
+                    {
+                        previousDateTime = currentDateTime;
+                    }
+                    
+                    
+                    if (responseMessage.StatusCode == _httpStatusCode)
+                    {
+                        Console.WriteLine($"{previousDateTime.ToString("yyyy/MM/dd hh:mm:ss")} HTTP Connect to {Host} ({Ip}) success");
+                        Console.WriteLine($"{currentDateTime.ToString("yyyy/MM/dd hh:mm:ss")} HTTP Connect to {Host} ({Ip}) success");
+                        _logger.WriteLog($"{previousDateTime.ToString("yyyy/MM/dd hh:mm:ss")} HTTP Connect to {Host} ({Ip}) success");
+                        _logger.WriteLog($"{currentDateTime.ToString("yyyy/MM/dd hh:mm:ss")} HTTP Connect to {Host} ({Ip}) success");
+                    }
+                    /*
                     else
                     {
                         Console.WriteLine($"{now.ToString("yyyy/MM/dd hh:mm:ss")} HTTP Connect to {Host} ({Ip}) fail");
                         _logger.WriteLog($"{now.ToString("yyyy/MM/dd hh:mm:ss")} HTTP Connect to {Host} ({Ip}) fail");
                     }
+                    */
     
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine($"{now.ToString("yyyy/MM/dd hh:mm:ss")} HTTP Connect to {Host} error");
-                    _logger.WriteLog($"{now.ToString("yyyy/MM/dd hh:mm:ss")} HTTP Connect to {Host} error");
+                    Console.WriteLine($"{currentDateTime.ToString("yyyy/MM/dd hh:mm:ss")} HTTP Connect to {Host} error");
+                    _logger.WriteLog($"{currentDateTime.ToString("yyyy/MM/dd hh:mm:ss")} HTTP Connect to {Host} error");
                 }
             }
         }
